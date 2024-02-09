72e04d900d1885023149579ddf5d57f5533f41eb
Halit Akca <halitakca070@gmail.com>
2024-02-09 15:57:23 +0300

    First Commit

--- app/src/main/java/com/example/jetpackcompose_mvmmcontactsapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_mvmmcontactsapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.jetpackcompose_mvmmcontactsapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,18 +26,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Sayfa(){
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Contacts") })
+        },
+        content = {
+
+        },
     )
 }
 
@@ -41,6 +51,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JetpackComposeMVMMContactsAppTheme {
-        Greeting("Android")
+        Sayfa()
     }
 }
